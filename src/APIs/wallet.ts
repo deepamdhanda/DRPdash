e3ff31304aad2e95164f8a520144575e412d8e30
Jasmeet Singh <jasmeetsingh4real1984@gmail.com>
2025-05-25 23:36:01 +0530

    added wallet recharges screen

--- src/APIs/wallet.ts
+++ src/APIs/wallet.ts
@@ -1,111 +1,145 @@
 import { toast } from "react-toastify";
 import { appAxios } from "../axios/appAxios";
 import { wallets_url, walletRecharge_url } from "../URLs/dash";
-import { Wallet } from "../screens/dashboard/Wallets";
-
+import { TWalletRecharge, Wallet } from "../screens/dashboard/Wallets";
 
 // Declare Razorpay on the window object
 declare global {
-    interface Window {
-        Razorpay: any;
-    }
+  interface Window {
+    Razorpay: any;
+  }
 }
+interface IWalletrechargeResponse {
+  data: TWalletRecharge[];
+  pagination: {
+    limit: number;
+    page: number;
+    pages: number;
+    total: number;
+  };
+}
+
 export const getAllWallets = async () => {
-    try {
-        const response = await appAxios.get(wallets_url, {
-            withCredentials: true,
-        });
-        return response.data as Wallet[];
-    } catch (error: any) {
-        toast.error("Failed to fetch wallets.");
-        throw error;
-    }
+  try {
+    const response = await appAxios.get(wallets_url, {
+      withCredentials: true,
+    });
+    return response.data as Wallet[];
+  } catch (error: any) {
+    toast.error("Failed to fetch wallets.");
+    throw error;
+  }
+};
+export const getAllWalletsRecharges = async (
+  page: number,
+  limit: number,
+  transactionId = "",
+  startDate: Date | null = null,
+  endDate: Date | null = null
+) => {
+  try {
+    const response = await appAxios.get(walletRecharge_url, {
+      params: {
+        page,
+        limit,
+        wallet_rechargeId: transactionId,
+        startDate,
+        endDate,
+      },
+    });
+    return response.data as IWalletrechargeResponse;
+  } catch (error: any) {
+    toast.error("Failed to fetch wallets.");
+    throw error;
+  }
 };
 
 export const verifyPayment = async (wallet_recharge_id: any, response: any) => {
-    try {
-        const response1 = await appAxios.put(walletRecharge_url + `/${wallet_recharge_id}`, response);
-        console.log("Payment response:", response1.data);
-    }
-    catch (error: any) {
-        console.error("Error updating payment:", error);
-        toast.error("Failed to update payment.");
-    }
-}
+  try {
+    const response1 = await appAxios.put(
+      walletRecharge_url + `/${wallet_recharge_id}`,
+      response
+    );
+    console.log("Payment response:", response1.data);
+  } catch (error: any) {
+    console.error("Error updating payment:", error);
+    toast.error("Failed to update payment.");
+  }
+};
 
 export const makePayment = async (amount: any, pool: any) => {
-    try {
-        amount = Math.round(amount * 100); // Convert to paise (1 INR = 100 paise)
-        const currency = 'INR'; // Assuming INR for Indian Rupees
-        const response = await appAxios.post(walletRecharge_url, {
-            amount: amount, // Amount in rupees
-            currency: currency,
-            pool_id: pool, // Pool name or ID
-        });
-        const { _id: wallet_recharge_id, razorpay_order_id: order_id } = response.data;
+  try {
+    amount = Math.round(amount * 100); // Convert to paise (1 INR = 100 paise)
+    const currency = "INR"; // Assuming INR for Indian Rupees
+    const response = await appAxios.post(walletRecharge_url, {
+      amount: amount, // Amount in rupees
+      currency: currency,
+      pool_id: pool, // Pool name or ID
+    });
+    const { _id: wallet_recharge_id, razorpay_order_id: order_id } =
+      response.data;
 
-        // Set up RazorPay options
-        const options = {
-            key: "rzp_live_stL91oujHW3VLL", // Replace with your RazorPay Key ID
-            amount: amount,
-            currency: currency,
-            name: "OrderzUp",
-            description: `Add ₹${amount} to ${pool}`,
-            image: "https://orderzup.com/wp-content/uploads/2025/05/logo-orderzup-real.png",
-            order_id: order_id,
-            handler: async (response: any) => {
-                verifyPayment(wallet_recharge_id, response);
-            },
-            prefill: {
-                name: "John Doe",
-                email: "john.doe@example.com",
-                contact: "9999999999",
-            },
-            theme: {
-                color: "#3399cc",
-            },
-        };
+    // Set up RazorPay options
+    const options = {
+      key: "rzp_live_stL91oujHW3VLL", // Replace with your RazorPay Key ID
+      amount: amount,
+      currency: currency,
+      name: "OrderzUp",
+      description: `Add ₹${amount} to ${pool}`,
+      image:
+        "https://orderzup.com/wp-content/uploads/2025/05/logo-orderzup-real.png",
+      order_id: order_id,
+      handler: async (response: any) => {
+        verifyPayment(wallet_recharge_id, response);
+      },
+      prefill: {
+        name: "John Doe",
+        email: "john.doe@example.com",
+        contact: "9999999999",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
 
-        const paymentObject = new window.Razorpay(options);
-        paymentObject.open();
-
-    }
-    catch (error: any) {
-        toast.error("Failed to make payment.");
-        throw error;
-    }
-}
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  } catch (error: any) {
+    toast.error("Failed to make payment.");
+    throw error;
+  }
+};
 
 export const createWallet = async (data: any) => {
-    try {
-        // console.log(data)
-        // return 0;
-        const response = await appAxios.post(wallets_url, data);
-        toast.success("Wallet created successfully!");
-        return response.data;
-    } catch (error: any) {
-        toast.error("Failed to create wallet.");
-        throw error;
-    }
+  try {
+    // console.log(data)
+    // return 0;
+    const response = await appAxios.post(wallets_url, data);
+    toast.success("Wallet created successfully!");
+    return response.data;
+  } catch (error: any) {
+    toast.error("Failed to create wallet.");
+    throw error;
+  }
 };
 
 export const updateWallet = async (id: string, data: any) => {
-    try {
-        const response = await appAxios.patch(`${wallets_url}/${id}`, data);
-        toast.success("Wallet updated successfully!");
-        return response.data;
-    } catch (error: any) {
-        toast.error("Failed to update wallet.");
-        throw error;
-    }
+  try {
+    const response = await appAxios.patch(`${wallets_url}/${id}`, data);
+    toast.success("Wallet updated successfully!");
+    return response.data;
+  } catch (error: any) {
+    toast.error("Failed to update wallet.");
+    throw error;
+  }
 };
 
 export const deleteWallet = async (id: string) => {
-    try {
-        await appAxios.delete(`${wallets_url}/${id}`);
-        toast.success("Wallet deleted successfully!");
-    } catch (error: any) {
-        toast.error("Failed to delete wallet.");
-        throw error;
-    }
+  try {
+    await appAxios.delete(`${wallets_url}/${id}`);
+    toast.success("Wallet deleted successfully!");
+  } catch (error: any) {
+    toast.error("Failed to delete wallet.");
+    throw error;
+  }
 };
--- src/screens/dashboard/Wallets.tsx
+++ src/screens/dashboard/Wallets.tsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { Button, Modal, Form } from "react-bootstrap";
-import DataTable from "react-data-table-component";
-import { getAllWallets, makePayment } from "../../APIs/wallet";
+import DataTable, { TableColumn } from "react-data-table-component";
+import {
+  getAllWallets,
+  getAllWalletsRecharges,
+  makePayment,
+} from "../../APIs/wallet";
 import { getAllPools } from "../../APIs/pool";
 import { Pool } from "./Pools";
+import moment from "moment";
 
 export interface User {
   _id: string;
@@ -32,20 +37,27 @@ export interface Wallet {
   _id: string;
 }
 
-const Wallets: React.FC = () => {
-  const [wallets, setWallets] = useState<Wallet[]>([]);
+enum EnumWalletTabs {
+  WALLET_TRANSACTIONS = "wallet_transactions",
+  WALLET_RECHARGE = "wallet_recharge",
+}
+export type TWalletRecharge = {
+  _id: string;
+  pool_id: string;
+  amount: number;
+  razorpay_order_id: string;
+  created_by: string;
+  status: "pending" | "paid" | "failed"; // Assuming these are possible statuses
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
+  razorpay_payment_id?: string;
+  full_details?: any;
+};
+
+const WalletTransactionsComponent = () => {
   const [loading, setLoading] = useState(true);
-  const [showModal, setShowModal] = useState(false);
-  const [amount, setAmount] = useState<number>(0);
-  const [selectedPool, setSelectedPool] = useState<string>("");
-  const [pools, setPools] = useState<Pool[]>([]);
-
-  useEffect(() => {
-    fetchWallets();
-    fetchPools();
-  }, []);
-
-  const fetchWallets = async () => {
+  const [wallets, setWallets] = useState<Wallet[]>([]);
+  const fetchWalletTransactions = async () => {
     try {
       setLoading(true);
       const data = await getAllWallets();
@@ -56,33 +68,6 @@ const Wallets: React.FC = () => {
       setLoading(false);
     }
   };
-
-  const fetchPools = async () => {
-    try {
-      setPools(await getAllPools());
-    } catch (error) {
-      console.error("Error fetching pools", error);
-    }
-  };
-
-  const handleAddMoney = () => {
-    setShowModal(true);
-  };
-
-  const handleModalClose = () => {
-    setShowModal(false);
-    setAmount(0);
-    setSelectedPool("");
-  };
-
-  const handlePayment = async () => {
-    try {
-      makePayment(amount, selectedPool)
-    } catch (error) {
-      console.error("Error during payment:", error);
-    }
-  };
-
 
   const columns = [
     {
@@ -145,9 +130,11 @@ const Wallets: React.FC = () => {
             Other:{" "}
             <span style={{ color: clr }}>
               {sign}₹
-              {(row.total_amount - row.freight_charge - row.cod_charges).toFixed(
-                2
-              )}
+              {(
+                row.total_amount -
+                row.freight_charge -
+                row.cod_charges
+              ).toFixed(2)}
             </span>{" "}
             <br />
           </div>
@@ -180,27 +167,25 @@ const Wallets: React.FC = () => {
       selector: (row: Wallet) =>
         row.createdAt
           ? new Date(row.createdAt).toLocaleDateString("en-IN", {
-            day: "2-digit",
-            month: "short",
-            year: "numeric",
-          })
+              day: "2-digit",
+              month: "short",
+              year: "numeric",
+            })
           : "—",
     },
   ];
-
+  useEffect(() => {
+    fetchWalletTransactions();
+  }, []);
   return (
-    <div className="container mt-4 ms-2 me-2">
-      <div className="d-flex justify-content-between align-items-center mb-3">
-        <h4>Wallets</h4>
-        <Button onClick={handleAddMoney}>+ Add Money</Button>
-      </div>
+    <>
       {loading ? (
         <p>Loading...</p>
       ) : wallets.length === 0 ? (
-        <p>No wallets found.</p>
+        <p>No wallets Transactions found.</p>
       ) : (
         <DataTable
-          title="Your Wallets"
+          title="Your Wallet Transactions"
           data={wallets}
           columns={columns as any}
           highlightOnHover
@@ -211,6 +196,238 @@ const Wallets: React.FC = () => {
           persistTableHead
           progressPending={wallets.length === 0}
         />
+      )}
+    </>
+  );
+};
+
+const WalletRechargeComponent = () => {
+  const [walletRecharges, setwalletRecharges] = useState<TWalletRecharge[]>([]);
+  const [pagination, setPagination] = useState({
+    page: 1,
+    limit: 10,
+    total: 0,
+  });
+  const [loading, setLoading] = useState(true);
+  const [transactionId, setTransactionId] = useState("");
+  const [dateRange, setDateRange] = useState({
+    from: moment().subtract(1, "month").toDate(),
+    to: moment().toDate(),
+  });
+  const columns: TableColumn<TWalletRecharge>[] = [
+    {
+      name: "#",
+      selector: (_row: any, index: any) => index + 1,
+      width: "60px",
+      sortable: true,
+    },
+    {
+      name: "Amount",
+      selector: (row: TWalletRecharge) => `₹${row.amount}`,
+      sortable: true,
+    },
+    {
+      name: "Status",
+      selector: (row: TWalletRecharge) => row.status,
+      sortable: true,
+      cell: (row: TWalletRecharge) => (
+        <span style={{ color: row.status === "paid" ? "green" : "orange" }}>
+          {row.status.toUpperCase()}
+        </span>
+      ),
+    },
+    {
+      name: "Order ID",
+      selector: (row: TWalletRecharge) => row._id,
+      sortable: true,
+    },
+    {
+      name: "Payment ID",
+      selector: (row: TWalletRecharge) => row.razorpay_payment_id ?? "—",
+    },
+    {
+      name: "Created At",
+      selector: (row: TWalletRecharge) =>
+        new Date(row.createdAt).toLocaleString(),
+    },
+    {
+      name: "Updated At",
+      selector: (row: TWalletRecharge) =>
+        new Date(row.updatedAt).toLocaleString(),
+    },
+  ];
+  const fetchWalletsRecharges = async (page: number, limit: number) => {
+    try {
+      setLoading(true);
+      const response = await getAllWalletsRecharges(
+        page,
+        limit,
+        transactionId,
+        dateRange.from,
+        dateRange.to
+      );
+      if (response) {
+        setwalletRecharges(response.data);
+        setPagination(response.pagination);
+      }
+    } catch (error) {
+      console.error("Error fetching wallets", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Debounce only transactionId
+  useEffect(() => {
+    const timeoutId = setTimeout(
+      () => {
+        fetchWalletsRecharges(pagination.page, pagination.limit);
+      },
+      transactionId ? 500 : 0
+    );
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [transactionId]);
+
+  // Fetch immediately when dateRange changes
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <div className="d-flex gap-2 mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search Transaction ID"
+          value={transactionId}
+          onChange={(e) => setTransactionId(e.target.value)}
+        />
+
+        <label>Start Date</label>
+        <input
+          type="date"
+          className="form-control"
+          value={moment(dateRange.from).format("YYYY-MM-DD")}
+          onChange={(e) =>
+            setDateRange((old) => ({
+              ...old,
+              from: moment(e.target.value).toDate(),
+            }))
+          }
+        />
+
+        <label>End Date</label>
+        <input
+          type="date"
+          className="form-control"
+          value={moment(dateRange.to).format("YYYY-MM-DD")}
+          onChange={(e) =>
+            setDateRange((old) => ({
+              ...old,
+              to: moment(e.target.value).toDate(),
+            }))
+          }
+        />
+
+        <Button
+          onClick={() => {
+            fetchWalletsRecharges(1, pagination.limit);
+          }}
+        >
+          Search
+        </Button>
+      </div>
+      <DataTable
+        title="Wallet Recharges"
+        columns={columns}
+        data={walletRecharges}
+        progressPending={loading}
+        pagination
+        paginationServer
+        paginationTotalRows={pagination.limit}
+        paginationDefaultPage={pagination.page}
+        onChangePage={(newPage) => {
+          fetchWalletsRecharges(newPage, pagination.limit);
+        }}
+        onChangeRowsPerPage={(newPerPage, newPage) => {
+          fetchWalletsRecharges(newPage, newPerPage);
+        }}
+        striped
+        highlightOnHover
+        persistTableHead
+      />
+    </div>
+  );
+};
+
+const Wallets: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<EnumWalletTabs>(
+    EnumWalletTabs.WALLET_TRANSACTIONS
+  );
+  const [showModal, setShowModal] = useState(false);
+  const [amount, setAmount] = useState<number>(0);
+  const [selectedPool, setSelectedPool] = useState<string>("");
+  const [pools, setPools] = useState<Pool[]>([]);
+
+  const fetchPools = async () => {
+    try {
+      setPools(await getAllPools());
+    } catch (error) {
+      console.error("Error fetching pools", error);
+    }
+  };
+
+  const handleAddMoney = () => {
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    setAmount(0);
+    setSelectedPool("");
+  };
+
+  const handlePayment = async () => {
+    try {
+      makePayment(amount, selectedPool);
+    } catch (error) {
+      console.error("Error during payment:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPools();
+  }, []);
+
+  return (
+    <div className="container mt-4 ms-2 me-2">
+      <div className="d-flex justify-content-between align-items-center mb-3">
+        <div className="d-flex ">
+          {Object.entries(EnumWalletTabs).map(([tab_key, tab_value]) => {
+            return (
+              <div
+                role="button"
+                onClick={() => setActiveTab(tab_value)}
+                key={tab_key}
+                className={`me-3 fs-5 ${
+                  activeTab === tab_value
+                    ? "text-decoration-underline "
+                    : "text-secondary"
+                }`}
+              >
+                {tab_key.replace("_", " ")}
+              </div>
+            );
+          })}
+        </div>
+        <Button onClick={handleAddMoney}>+ Add Money</Button>
+      </div>
+      {activeTab === EnumWalletTabs.WALLET_TRANSACTIONS && (
+        <WalletTransactionsComponent />
+      )}
+      {activeTab === EnumWalletTabs.WALLET_RECHARGE && (
+        <WalletRechargeComponent />
       )}
 
       {/* Add Money Modal */}
@@ -238,7 +455,7 @@ const Wallets: React.FC = () => {
                 <option value="">Select a pool</option>
                 {pools.map((pool) => (
                   <option key={pool._id} value={pool._id}>
-                    {pool.name} (₹{(pool?.wallet_balance)?.toFixed(2) || 0})
+                    {pool.name} (₹{pool?.wallet_balance?.toFixed(2) || 0})
                   </option>
                 ))}
               </Form.Select>
